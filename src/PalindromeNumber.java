4d008624ae7172e1bed198a407b9120b968902f1
jin1105 <670522325@qq.com>
2017-04-24 22:38:42 +0800

    2017-4-24

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,17 @@
+/**
+ * Created by gaojin on 2017/4/24.
+ */
+public class PalindromeNumber {
+    public static void main(String[] args){
+        System.out.println(isPalindrome(1000));
+    }
+    public static boolean isPalindrome(int x) {
+        if (x<0 || (x!=0 && x%10==0)) return false;
+        int rev = 0;
+        while (x>rev){
+            rev = rev*10 + x%10;
+            x = x/10;
+        }
+        return (x==rev || x==rev/10);
+    }
+}
