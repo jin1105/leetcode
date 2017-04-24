d7fc08256bb539fdc210543d458a220160630e7e
jin1105 <670522325@qq.com>
2017-04-24 21:58:40 +0800

    2017-4-24

---
+++ src/ZigZag.java
@@ -0,0 +1,27 @@
+/**
+ * Created by gaojin on 2017/4/24.
+ */
+public class ZigZag {
+
+    public static void main(String[] args){
+        System.out.println(convert("PAYPALISHIRING", 3));
+
+    }
+    public static String convert(String s, int numRows) {
+        char[] c = s.toCharArray();
+        int len = c.length;
+        StringBuffer[] sb = new StringBuffer[numRows];
+        for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
+
+        int i = 0;
+        while (i < len) {
+            for (int idx = 0; idx < numRows && i < len; idx++) // vertically down
+                sb[idx].append(c[i++]);
+            for (int idx = numRows-2; idx >= 1 && i < len; idx--) // obliquely up
+                sb[idx].append(c[i++]);
+        }
+        for (int idx = 1; idx < sb.length; idx++)
+            sb[0].append(sb[idx]);
+        return sb[0].toString();
+    }
+}
---
+++ src/longestPalindrome.java
@@ -0,0 +1,31 @@
+/**
+ * Created by gaojin on 2017/4/24.
+ */
+public class longestPalindrome {
+    public static void main(String[] args){
+        System.out.println(longestPalindrome("abcda"));
+
+    }
+    public static String longestPalindrome(String s){
+        String res = "";
+        int currLength = 0;
+        for(int i=0;i<s.length();i++){
+            if(isPalindrome(s,i-currLength-1,i)){
+                res = s.substring(i-currLength-1,i+1);
+                currLength = currLength+2;
+            }
+            else if(isPalindrome(s,i-currLength,i)){
+                res = s.substring(i-currLength,i+1);
+                currLength = currLength+1;
+            }
+        }
+        return res;
+    }
+    public static boolean isPalindrome(String s, int begin, int end){
+        if(begin<0) return false;
+        while(begin<end){
+            if(s.charAt(begin++)!=s.charAt(end--)) return false;
+        }
+        return true;
+    }
+}
